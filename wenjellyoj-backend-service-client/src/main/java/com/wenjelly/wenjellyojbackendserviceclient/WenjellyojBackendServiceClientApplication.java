29b931eea5f6911cf5053509fcdcf821f67ee188
wenjelly_Yui <1445607419@qq.com>
2024-07-28 01:44:52 +0800

    更新service-client打包逻辑

--- wenjellyoj-backend-service-client/src/main/java/com/wenjelly/wenjellyojbackendserviceclient/WenjellyojBackendServiceClientApplication.java
+++ wenjellyoj-backend-service-client/src/main/java/com/wenjelly/wenjellyojbackendserviceclient/WenjellyojBackendServiceClientApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@EnableDiscoveryClient
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@EnableDiscoveryClient
 public class WenjellyojBackendServiceClientApplication {
 
     public static void main(String[] args) {
